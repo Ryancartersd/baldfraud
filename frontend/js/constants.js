1095fc5589058d4723c1e2f8b2b43c46e5fad176
Ryan Carter <ryancartersd@me.com>
2022-08-09 11:48:55 -0700

    rink update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x6b1050eC5B48746Ea2Ba29c99a5fD723C11b44FB"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Meet The Bald Frauds!";
+const welcome_h2 = "Welcome to the World of the Bald!";
+const welcome_p = 'Bald Frauds are the 1st release in a series of hand drawn fully decentralized NFT projects. Our main goal is to bring a fully functional meta-verse where the Bald Frauds represent a getaway from the day to day pressures of life.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
